a410e32273647672bc2e33513ff3d5c96fb852f7
erhu <erhu.cn@qq.com>
2015-02-15 09:56:55 +0800

    [erhu] add tests for BTree.

--- 1-树/0-二叉树/erhu/BTree.java
+++ 1-树/0-二叉树/erhu/BTree.java
@@ -8,19 +8,59 @@ public class BTree<T> {
         this.value = t;
     }
 
-    public void setLeft(BTree tree) {
+    public void setLeft(BTree<T> tree) {
         this.left = tree;
     }
 
-    public void setRight(BTree tree) {
+    public void setRight(BTree<T> tree) {
         this.right = tree;
     }
 
     /**
-     * 遍历二叉树
+     * main
      */
-    static class BTreeTraverse {
+    public static void main(String[] args) {
 
+        System.out.println("         Root              ");
+        System.out.println("         / \\              ");
+        System.out.println("        /   \\             ");
+        System.out.println("    1_left 1_right         ");
+        System.out.println("             / \\          ");
+        System.out.println("            /   \\         ");
+        System.out.println("       2_left  2_right\n\n");
+
+
+        BTree<String> tree = new BTree<String>("Root");
+        // left
+        tree.left = new BTree<String>("1_left");
+        // right
+        BTree<String> lvl1_right = new BTree<String>("1_right");
+        tree.right = lvl1_right;
+
+        lvl1_right.left = new BTree<String>("2_left");
+        lvl1_right.right = new BTree<String>("2_right");
+
+        System.out.println("preOrder:");
+        BTreeTraverse.preOrder(tree);
+        System.out.println();
+
+        System.out.println("inOrder:");
+        BTreeTraverse.inOrder(tree);
+        System.out.println();
+
+        System.out.println("postOrder:");
+        BTreeTraverse.postOrder(tree);
+        System.out.println();
+    }
+
+    /**
+     * traverse tool
+     */
+    private static class BTreeTraverse {
+
+        /**
+         * preOrder
+         */
         public static void preOrder(BTree t) {
             printNode(t);
             if (t.left != null) {
@@ -31,6 +71,9 @@ public class BTree<T> {
             }
         }
 
+        /**
+         * inOrder
+         */
         public static void inOrder(BTree t) {
             if (t.left != null) {
                 inOrder(t.left);
@@ -41,7 +84,9 @@ public class BTree<T> {
             }
         }
 
-
+        /**
+         * postOrder
+         */
         public static void postOrder(BTree t) {
             if (t.left != null) {
                 postOrder(t.left);
@@ -51,7 +96,6 @@ public class BTree<T> {
             }
             printNode(t);
         }
-
 
         private static void printNode(BTree t) {
             System.out.println(t.value.toString());
